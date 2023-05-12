6902588a3e7e747a0b33ca56558b8a3e006ed233
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-12 17:21:18 +0700

    Make library instead of executablee

--- Package.swift
+++ Package.swift
@@ -4,19 +4,25 @@ import PackageDescription
 
 let package = Package(
     name: "NimbleFastlane",
+    products: [
+            .library(
+                name: "NimbleFastlane",
+                targets: ["NimbleFastlane"]
+            )
+        ],
     dependencies: [
         .package(url: "https://github.com/blyscuit/fastlane-swift-group", branch: "custom-plugin"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .executableTarget(
+        .target(
             name: "NimbleFastlane",
             dependencies: [
                 .product(name: "Fastlane", package: "fastlane-swift-group")
-            ]),
+            ]
+        ),
         .testTarget(
             name: "NimbleFastlaneTests",
-            dependencies: ["NimbleFastlane"]),
+            dependencies: ["NimbleFastlane"]
+        )
     ]
 )
