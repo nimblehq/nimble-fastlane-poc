5cfc9ecdd9b0d5211bb8d44c4af99525598b4042
Bliss Pisit Wetcha <pisit@nimblehq.co>
2023-05-15 10:10:21 +0700

    Move firebase distribution to Fastlane layer

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             )
         ],
     dependencies: [
-        .package(url: "https://github.com/blyscuit/fastlane-swift-group", branch: "custom-plugin"),
+        .package(url: "https://github.com/blyscuit/fastlane-swift-group", branch: "firebase-distribution-plugin"),
     ],
     targets: [
         .target(
