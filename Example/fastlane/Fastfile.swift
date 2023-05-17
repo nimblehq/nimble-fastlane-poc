094b2254dff87882e3ba54ec685552088abde55a
Su Ho <su.v@nimblehq.co>
2023-05-17 08:56:31 +0700

    .

--- Example/fastlane/Fastfile.swift
+++ Example/fastlane/Fastfile.swift
@@ -6,12 +6,45 @@
 //     https://docs.fastlane.tools/actions
 //
 
-import Foundation
 import Algorithms
+import Foundation
+import NimbleFastlane
+
+// MARK: - Fastfile
 
 class Fastfile: LaneFile {
-	func customLane() {
-	desc("Description of what the lane does")
-		// add actions here: https://docs.fastlane.tools/actions
-	}
+
+    var firebase: NimbleFastlane.Firebase! = FirebaseImplementation()
+
+    func customLane() {
+        desc("Description of what the lane does")
+        // add actions here: https://docs.fastlane.tools/actions
+    }
+}
+
+// MARK: NewSharedFastfile
+
+extension Fastfile: NewSharedFastfile {}
+
+// MARK: - FirebaseImplementation
+
+class FirebaseImplementation: Firebase {
+
+    func firebaseAppDistribution(
+        ipaPath: String,
+        app: String,
+        groups: String,
+        releaseNotes: String,
+        firebaseCliToken: String,
+        debug: Bool
+    ) {
+        FastlaneRunner.firebaseAppDistribution(
+            ipaPath: .userDefined(ipaPath),
+            app: .userDefined(app),
+            groups: .userDefined(groups),
+            releaseNotes: .userDefined(releaseNotes),
+            firebaseCliToken: .userDefined(firebaseCliToken),
+            debug: .userDefined(debug)
+        )
+    }
 }
