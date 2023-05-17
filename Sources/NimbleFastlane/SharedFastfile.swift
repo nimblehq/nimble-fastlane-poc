3c6f28de33709ed7a43025bbb94679965ded5f16
Su Ho <su.v@nimblehq.co>
2023-05-17 08:40:02 +0700

    Improve fastfile

--- Sources/NimbleFastlane/SharedFastfile.swift
+++ Sources/NimbleFastlane/SharedFastfile.swift
@@ -1,14 +1,50 @@
 import Fastlane
 
-public class SharedFastfile: LaneFile {
+// MARK: - SharedFastfile
 
-    public func getIsCILane() {
+public protocol SharedFastfile: AnyObject {
+
+    var firebaseProtocol: FirebaseAppDistributionProtocol! { get set }
+
+    func getIsCILane()
+    func getVersionNumberLane()
+    func uploadToFirebaseLane()
+}
+
+public extension SharedFastfile {
+
+    func getIsCILane() {
         puts(message: .userDefined("getIsCILane"))
         puts(message: .userDefined(EnvironmentParser.string(key: "IS_CI")))
     }
 
-    public func getVersionNumberLane() {
+    func getVersionNumberLane() {
         puts(message: .userDefined("getVersionNumberLane"))
         puts(message: .userDefined(Version.versionNumber))
     }
+
+    func uploadToFirebaseLane() {
+        firebaseProtocol.firebaseAppDistribution(
+            ipaPath: "",
+            app: "",
+            groups: "",
+            releaseNotes: "",
+            firebaseCliToken: "",
+            debug: true
+        )
+    }
+}
+
+// MARK: - FirebaseAppDistributionProtocol
+
+public protocol FirebaseAppDistributionProtocol {
+
+    func firebaseAppDistribution(
+        ipaPath: String,
+        app: String,
+        groups: String,
+        releaseNotes: String,
+        firebaseCliToken: String,
+        debug: Bool
+    )
 }
