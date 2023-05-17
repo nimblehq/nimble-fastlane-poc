d624af582eacc5a2beeb0b2cdf41376e394c3b55
Su Ho <su.v@nimblehq.co>
2023-05-17 08:43:53 +0700

    Init fastlane swift with plugin

--- Example/FastlaneRunner/Sources/main.swift
+++
@@ -1,8 +0,0 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
-
-import Fastlane
-import NimbleFastlane
-
-Configuration.setUp(target: "Example", xcodeprojPath: "Example.xcodeproj")
-Main().run(with: SharedFastfile())
